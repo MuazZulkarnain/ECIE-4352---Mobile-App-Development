25d48c59570bea3b2dc4d1be9d97f33a04b8d357
Syaamil Aris <81031528+syaamilaris@users.noreply.github.com>
2022-10-31 12:23:32 +0800

    Update App.js

--- Assignment 3/App.js
+++ Assignment 3/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
+const Cat = (props) => {
+  const [isAwake, setIsAwake] = useState(true);
+  //const i = 0;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        My name is {props.name}, and I am {isAwake ? "awake. Day 0" : "sleeping. Day 1"}
+      </Text>
+      <Button
+        onPress={() => {
+          setIsAwake(true);
+        }}
+        title={isAwake ? "Let me sleep" : "Wake me up"}
+      />
     </View>
   );
-}
+};
+
+const Cafe = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://static.vecteezy.com/system/resources/thumbnails/007/731/064/small/cat-muzzle-cat-face-simple-illustration-vector.jpg",
+        }}
+        style={{ width: 300, height: 200 }}
+      />
+      <Cat name="Comel" />
+      <Cat name="Oyen" />
+      <Cat name="Mika" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFE5B4",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innerText: {
+    color: "red",
   },
 });
+
+export default Cafe;
