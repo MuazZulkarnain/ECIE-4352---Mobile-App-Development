9309c1961608716e102a8ec62eb242258172026f
Syaamil Aris <81031528+syaamilaris@users.noreply.github.com>
2023-01-14 22:48:28 +0800

    Update App.js

--- PROJECT/App.js
+++ PROJECT/App.js
@@ -2,9 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
+const styles = StyleSheet.create({
 
-      <Text>
-        TEMPLATE
-      </Text>
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+
+});
+
+const FlatListBasics = () => {
+  const [text, setText] = useState("");
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={{ height: 40, borderWidth: 1, fontSize: 18 }}
+        placeholder="Insert several names 📛"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+
+      <FlatList
+        data={text.split(" ")}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+      />
+    </View>
+  );
 };
+
+export default FlatListBasics;
