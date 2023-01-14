73482e1100f33192cf9cd0c4a3c3fe1f33e65035
Syaamil Aris <81031528+syaamilaris@users.noreply.github.com>
2023-01-15 01:16:36 +0800

    asd

--- PROJECT/screens/about.js
+++
@@ -1,5 +0,0 @@
-import { createStatckNavigator } from 'react-navigation-stack';
-import { createAppContainer } from "react-navigation";
-import Home from '../screens/home';
-
-const screens 
--- PROJECT/screens/home.js
+++
@@ -1,5 +0,0 @@
-import { createStatckNavigator } from 'react-navigation-stack';
-import { createAppContainer } from "react-navigation";
-import Home from './home';
-
-const screens 
