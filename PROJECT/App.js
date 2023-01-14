5ca67126553c1600dd07a14269e0faf9089fc66b
Syaamil Aris <81031528+syaamilaris@users.noreply.github.com>
2023-01-14 22:45:37 +0800

    Update App.js

--- PROJECT/App.js
+++ PROJECT/App.js
@@ -4,60 +4,7 @@ import { FlatList, StyleSheet, Text, View, TextInput, Button } from 'react-nativ
 
 export default function App() {
 
-  const textInputChange = (t) => {setTextInputData(t)}
-  const buttonClicked = () => {setMyData(current => [...current, textInputData])}
-  const [myData, setMyData] = useState(['Apple', 'Orange'])
-  const [textInputData, setTextInputData] = useState('')
-
-  return (
-    <View style = {styles.container}>
-      <Text style = {styles.textPrompt}>
-        ENTER ITEM
+      <Text>
+        TEMPLATE
       </Text>
-      <StatusBar style = "auto"/>
-      <TextInput 
-        style = {styles.inputText}
-        placeholder = "Enter fruit name"
-        onChangeText={(newText) => textInputChange(newText)}
-      />
-      <Text> </Text>
-      <View>
-        <Button
-          title = "Add"
-          onPress={buttonClicked}
-          />
-        <Text> </Text>
-        <Text style = {styles.textPrompt}>
-          The list of fruits:
-        </Text>
-        <Text> </Text>
-        <FlatList
-          data={myData}
-          renderItem={({item}) => <Text>{item}</Text>}
-        />      
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-
-  inputText: {
-    marginTop:15,
-    borderWidth: 3,
-    borderColor:"white"
-  },
-
-  container: {
-    flex: 1,
-    backgroundColor: '#ffdb96',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  textPrompt: {
-    fontSize: 25,
-    color: "purple",
-  },
-
-});
+};
